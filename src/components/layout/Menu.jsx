ec1388acc616e675e9b250e5edb31ba4d320ae97
jaisonrobson <xd_jrg@hotmail.com>
2021-08-24 18:49:52 -0300

    Aplicado estilizacao aos componentes Menu e Content & utilizado os mesmos em App.jsx #Aula53

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 
 const Menu = props => (
     <aside className="Menu">
-        Componente Menu
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Inicio</a>
+                </li>
+                <li>
+                    <a href="/">Sobre</a>
+                </li>
+            </ul>
+        </nav>
     </aside>
 )
 
